689afb6eb6ecca3d283d730351dade6244435ea8
Andrey Orlov <aorlov@mirantis.com>
2013-12-02 18:36:27 +0400

    generic relay command

--- Pathfinder/RelayCommand.cs
+++ Pathfinder/RelayCommand.cs
@@ -3,10 +3,11 @@ using System.Windows.Input;
 
 namespace Pathfinder
 {
-	internal class RelayCommand : ICommand
+	internal class RelayCommand<T> : ICommand
 	{
-		private readonly Action _action;
-		public RelayCommand(Action action)
+		private readonly Action<T> _action;
+
+		public RelayCommand(Action<T> action)
 		{
 			_action = action;
 		}
@@ -19,6 +20,27 @@ namespace Pathfinder
 		public void Execute(object parameter)
 		{
 			if(CanExecute(parameter))
+				_action((T)parameter);
+		}
+		public event EventHandler CanExecuteChanged;
+	}
+
+	internal class RelayCommand : ICommand
+	{
+		private readonly Action _action;
+
+		public RelayCommand(Action action)
+		{
+			_action = action;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+
+		public void Execute(object parameter)
+		{
 				_action();
 		}
 		public event EventHandler CanExecuteChanged;
